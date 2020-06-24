a15a4bc53d9b985a17d015bfe8a7255ee60b674c
Jacob Kelly <jacob.jin.kelly@gmail.com>
2020-06-24 16:12:52 -0400

    clean up job scrpt

--- launch_job.sh
+++ launch_job.sh
@@ -7,12 +7,12 @@ file=$4
 args=$5 # specify reg and lam
 q=$6  # TODO: high, deadline (need account), normal
 resource=1
-ssd=/scratch/ssd001/home/$USER/jessebett/jax/examples  # TODO(jesse): set this to where ffjord.py file is
-j_dir=$ssd/vaughan2/$d/$j_name
+ssd=$(pwd)  # TODO: set to directory where jobs will be stored
+j_dir=$ssd/$d/$j_name
 
 mkdir -p $j_dir/scripts
 
-# TODO: do we really need 4 cpus?
+# TODO: CPU num and GPU mem
 # build slurm script
 mkdir -p $j_dir/log
 echo "#!/bin/bash
@@ -31,7 +31,6 @@ bash ${j_dir}/scripts/${j_name}.sh
 
 # build bash script
 
-# TODO: this is for running regular training jobs
 echo -n "#!/bin/bash
 . /h/jkelly/new_jet_nodes.env
 touch /checkpoint/$USER/\$SLURM_JOB_ID/DELAYPURGE
@@ -39,24 +38,8 @@ python $file $args --seed=0 --dirname=${j_dir} --ckpt_path=/checkpoint/$USER/\$S
 
 " > $j_dir/scripts/${j_name}.sh
 
-# TODO: this is only for plot version!
-# echo -n "#!/bin/bash
-# . /h/jkelly/new_jet_nodes.env
-# touch /checkpoint/$USER/\$SLURM_JOB_ID/DELAYPURGE
-# python $file $args
-# 
-# " > $j_dir/scripts/${j_name}.sh
-
-# TODO: this is for checking GPU shit
-# echo -n "#!/bin/bash
-# . /h/jkelly/new_jet_nodes.env
-# touch /checkpoint/$USER/\$SLURM_JOB_ID/DELAYPURGE
-# nvidia-smi
-# " > $j_dir/scripts/${j_name}.sh
-
 cp $file $j_dir/scripts/
 
-# TODO: deadline or nah or v1
+# TODO: fix this to automatically add the deadline account when needed
 # sbatch $j_dir/scripts/${j_name}.slrm --qos $q --account deadline
-# sbatch $j_dir/scripts/${j_name}.slrm --qos $q
-sbatch $j_dir/scripts/${j_name}.slrm 
+sbatch $j_dir/scripts/${j_name}.slrm --qos $q
