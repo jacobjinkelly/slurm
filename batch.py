537dc16e7037387104a6013622924d1c13405b3c
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-06-09 09:43:13 -0400

    remove

--- batch.py
+++ batch.py
@@ -13,11 +13,15 @@ assert sys.version_info.minor >= 6
 def get_args():
     parser = argparse.ArgumentParser("Launch a sweep of jobs")
 
+    # arguments that will be set for most sweeps
     parser.add_argument("-p", "--partition", type=str, default="t4v2")
     parser.add_argument("-j", "--j_name", type=str, required=True)
     parser.add_argument("-f", "--file", type=str, required=True)
     parser.add_argument("-a", "--args", type=str)
     parser.add_argument("-q", "--q", type=str, required=True)
+
+    # default arguments that will rarely be changed
+    parser.add_argument("--experiment_dir", type=str, default="experiments")
 
     return parser.parse_args()
 
