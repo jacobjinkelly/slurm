1af6aec3cd5ca815ce983d998b11a04116000049
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-06-09 09:22:33 -0400

    format

--- batch.py
+++ batch.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
 import sys
+import argparse
 
 print("Hello world")
 
 assert sys.version_info.major == 3
 assert sys.version_info.minor > 6
+
+
+def main():
+    parser = argparse.ArgumentParser("Launch a sweep of jobs")
+
+
+if __name__ == "__main__":
+    main()
