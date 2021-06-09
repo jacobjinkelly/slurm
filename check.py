8db7b3f502391c2203d0a0e266fde5c4dc2c438c
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-06-09 13:08:07 -0400

    check

--- check.py
+++ check.py
@@ -1,6 +1,23 @@
-"""
-Check the status of each job in a sweep.
-"""
+#!/usr/bin/python3
+
+import sys
+import argparse
+import glob
+
+assert sys.version_info.major == 3
+assert sys.version_info.minor >= 6
+
+def main():
+    parser = argparse.ArgumentParser("Check status of a sweep of jobs")
+
+    # arguments that need to be set for each sweep
+    parser.add_argument("-s", "--substr", type=str, default="")
+
+if __name__ == "__main__":
+    main()
+
+for log_file in
+
 for log in *${1}*/log.txt; do
     out=$(cat $log | tail -1)
     echo -e $out ' \t ' $log
