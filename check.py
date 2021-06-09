40cd96fddbbea899f8d092fac4b5a0e5ae9cdd98
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-06-09 13:10:25 -0400

    refactro

--- check.py
+++ check.py
@@ -7,18 +7,23 @@ import glob
 assert sys.version_info.major == 3
 assert sys.version_info.minor >= 6
 
-def main():
+
+def get_args():
     parser = argparse.ArgumentParser("Check status of a sweep of jobs")
 
     # arguments that need to be set for each sweep
     parser.add_argument("-s", "--substr", type=str, default="")
+    parser.add_argument("-f", "--file", type=str, default="log.txt")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    for log in glob(f"*{args.substr}/{args.file}"):
+        pass
+
 
 if __name__ == "__main__":
     main()
-
-for log_file in
-
-for log in *${1}*/log.txt; do
-    out=$(cat $log | tail -1)
-    echo -e $out ' \t ' $log
-done
