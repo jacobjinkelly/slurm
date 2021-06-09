52021acdad3aef68a7cf5604c1a889182ba9a539
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-06-09 14:26:51 -0400

    ckpt

--- batch.py
+++ batch.py
@@ -163,7 +163,7 @@ def launch_sweep(args):
         j_name = get_j_name(sweep_arg, sweep_keys)
         j_args = fixed_args + get_j_args(sweep_arg, sweep_keys)
         launch_job(args.exp_dir, args.partition, j_name, args.file, j_args, args.q,
-                   args.env, args.resource, args.cpus_per_task, args.mem, args.exclude,
+                   args.ckpt, args.env, args.resource, args.cpus_per_task, args.mem, args.exclude,
                    args.ntasks_per_node, args.nodes)
 
 
