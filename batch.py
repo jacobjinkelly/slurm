00716ca614bc83ae7cc3cca0ef23a12477b11c9f
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-06-09 09:29:25 -0400

    format

--- batch.py
+++ batch.py
@@ -10,11 +10,11 @@ assert sys.version_info.minor >= 6
 def main():
     parser = argparse.ArgumentParser("Launch a sweep of jobs")
 
-    parser.add_argument("--partition", type=str, default="t4v2")
-    parser.add_argument("--j_name", type=str, required=True)
-    parser.add_argument("--file", type=str, required=True)
-    parser.add_argument("--args", type=str)
-    parser.add_argument("--q", type=str, required=True)
+    parser.add_argument("-p", "--partition", type=str, default="t4v2")
+    parser.add_argument("-j", "--j_name", type=str, required=True)
+    parser.add_argument("-f", "--file", type=str, required=True)
+    parser.add_argument("-a", "--args", type=str)
+    parser.add_argument("-q", "--q", type=str, required=True)
 
     args = parser.parse_args()
 
