4c6481415f9ca79a0fe39971553194b4827e00c1
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-06-09 09:25:34 -0400

    format

--- batch.py
+++ batch.py
@@ -15,6 +15,8 @@ def main():
     parser.add_argument("--partition", type=str, default="t4v2")
     parser.add_argument("--j_name", type=str, required=True)
     parser.add_argument("--file", type=str, required=True)
+    parser.add_argument("--args", type=str)
+    parser.add_argument("--q", type=str, required=True)
 
 
 if __name__ == "__main__":
