b43c246bad749e7461f5a9a52b5c0d26573fcbf4
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-07-07 17:03:56 -0400

    add

--- get.py
+++ get.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument("-t", "--timestamp", type=str, required=True, help="Timestamp of experiment")
     parser.add_argument("-r", "--root", type=str, default="/h/jkelly/kfac-pytorch/experiments/",
                         help="Remote root directory")
-    parser.add_argument("-h", "--host", type=str, default="vd", help="SSH host")
+    parser.add_argument("-s", "--ssh", type=str, default="vd", help="SSH host")
     parser.add_argument("-e", "--exclude", type=str, default="*.pt", help="Exclude files matching this regex")
 
     args = parser.parse_args()
@@ -20,7 +20,7 @@ def main():
     # -e: use ssh for encryption
     # --exclude: exclude all checkpoint files (large and unnecessary)
 
-    cmd = f"rsync -av -e ssh --exclude='{args.exclude}' {args.host}:{args.root}/{args.timestamp}/ {args.timestamp}/"
+    cmd = f"rsync -av -e ssh --exclude='{args.exclude}' {args.ssh}:{args.root}/{args.timestamp}/ {args.timestamp}/"
     subprocess.run(cmd, shell=True, check=True)
 
 
