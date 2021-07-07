92fce4cabf9fd0d652dc2682b3a0ad000ddedc5b
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-07-07 16:38:21 -0400

    get

--- get.py
+++ get.py
@@ -11,6 +11,11 @@ def main():
 
     args = parser.parse_args()
 
+    # -a: recurse into subdirectories, turn on archive mode and all other options
+    # -v: verbose
+    # -e: use ssh for encryption
+    # --exclude: exclude all checkpoint files (large and unnecessary)
+
     cmd = "rsync -av -e ssh --exclude='*.pt' vd:/h/jkelly/zero-shot/experiments/${exp_dir}/ ${exp_dir}/"
     subprocess.run(cmd, shell=True, check=True)
 
