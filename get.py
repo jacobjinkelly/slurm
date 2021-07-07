e25c94fa6f4b11891aec3b702daf9cd34680b19e
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-07-07 16:36:40 -0400

    get

--- get.py
+++ get.py
@@ -7,6 +7,8 @@ import subprocess
 def main():
     parser = argparse.ArgumentParser("Download an experiment directory")
 
+    parser.add_argument("-t", "--timestamp", type=str, required=True, help="Timestamp of experiment")
+
 
 if __name__ == "__main__":
     main()
