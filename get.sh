0a3adc9d48bbcca183bcaf11af73770876e87e41
Jacob Kelly <jacob.jin.kelly@gmail.com>
2021-07-12 13:47:28 -0400

    rm

--- get.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-exp_dir=${1}  # timestamp directory of experiment
-
-# -a: recurse into subdirectories, turn on archive mode and all other options
-# -v: verbose
-# -e: use ssh for encryption
-# --exclude: exclude all checkpoint files (large and unnecessary)
-rsync -av -e ssh --exclude='*.pt' vd:/h/jkelly/zero-shot/experiments/${exp_dir}/ ${exp_dir}/
